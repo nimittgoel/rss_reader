9901b27d6c8cbdea285161ffa668deaa6d21b5e1
nimittgoel <84322727+nimittgoel@users.noreply.github.com>
2022-06-01 21:02:54 +0530

    This file contains the structure of classes with the required format used in the script.

---
+++ RSS_data_struc.rb
@@ -0,0 +1,73 @@
+  class RSS_Channel
+
+    def Title=(x)
+      @Title = x
+    end
+    def Title
+      @Title      
+    end
+  
+    def Description=(x)
+      @Description = x
+    end
+    def Description
+      @Description      
+    end
+  
+    def Link=(x)
+      @Link = x
+    end
+    def Link
+      @Link      
+    end
+  
+    def PubDate=(x)
+      @PubDate = x
+    end
+    def PubDate
+      @PubDate      
+    end
+  
+    def RssItems=(x)
+      @RssItems = x
+    end
+    def RssItems
+      @RssItems      
+    end
+  
+  end
+  
+  class RSS_Item
+    
+      def Title=(x)
+        @Title = x
+      end
+      def Title
+        @Title      
+      end
+    
+      def Description=(x)
+        @Description = x
+      end
+      def Description
+        @Description      
+      end
+    
+      def Link=(x)
+        @Link = x
+      end
+      def Link
+        @Link      
+      end
+    
+      def PubDate=(x)
+        @PubDate = x
+      end
+      def PubDate
+        @PubDate      
+      end
+  
+  end
+  
+  
+  
